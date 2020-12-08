ddf5c9d136a6b3e5b076a2981c8afce9bc201be2
Ashley J. Robinson <ashley181291@gmail.com>
2020-12-08 18:53:15 +0000

    smaller flywheel

--- trials/20201205_flywheel/gen.py
+++ trials/20201205_flywheel/gen.py
@@ -16,31 +16,32 @@ App.ActiveDocument=App.getDocument(name)
 Gui.ActiveDocument=Gui.getDocument(name)
 
 # Main wheel
-wheel_depth   = 5
-ext_wheel_rad = 50
-int_wheel_rad = 40
+wheel_depth   = 12
+ext_wheel_rad = 25
+int_wheel_rad = 20
 
 ext_wheel=Part.makeCylinder(ext_wheel_rad, wheel_depth)
 int_wheel=Part.makeCylinder(int_wheel_rad, wheel_depth)
 wheel=ext_wheel.cut(int_wheel)
 
 # Prongs
-prong_width=15
-x=Part.makeBox(2*int_wheel_rad,prong_width,wheel_depth)
+prong_width=12
+prong_depth=3
+x=Part.makeBox(2*int_wheel_rad,prong_width,prong_depth)
 x.translate(Base.Vector(-int_wheel_rad,-(prong_width/2),0))
 wheel=Part.makeCompound([wheel,x])
 
-y=Part.makeBox(2*int_wheel_rad,prong_width,wheel_depth)
+y=Part.makeBox(2*int_wheel_rad,prong_width,prong_depth)
 y.translate(Base.Vector(-int_wheel_rad,-(prong_width/2),0))
 y.rotate(Base.Vector(0, 0, 0),Base.Vector(0, 0, 1), 90)
 wheel=Part.makeCompound([wheel,y])
 
 # Main wheel
-stick_depth = 5
+stick_depth = 10
 stick_rad   = 8
 
 stick=Part.makeCylinder(stick_rad, stick_depth)
-stick.translate(Base.Vector(0,0,wheel_depth))
+stick.translate(Base.Vector(0,0,prong_depth))
 wheel=Part.makeCompound([wheel,stick])
 
 # Star shapes to cut out
