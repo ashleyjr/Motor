a2d7e89bd0850bac3fdb9c606e740834474e02f4
ashleyjr <ashley181291@gmail.com>
2020-12-14 22:43:44 +0000

    basic waves output

--- sw/mcu/src/main.c
+++ sw/mcu/src/main.c
@@ -9,7 +9,18 @@
 // Defines
 //-----------------------------------------------------------------------------
 
-#define PSTEP 10
+#define FREQ_TUNE    7990000
+#define FREQ_MAX     100000
+#define FREQ_MIN     100
+#define FREQ_STEP    100
+#define DUTY_MAX     100000
+#define DUTY_MIN     100
+#define DUTY_STEP    100
+#define PHASE_MAX    360000
+#define PHASE_MIN    0
+#define PHASE_STEP   1000
+
+
 
 SBIT(LED0, SFR_P1, 0);  
 SBIT(LED1, SFR_P1, 1);  
@@ -30,12 +41,17 @@ U32   uartTxPos(U32 num, U8 pos);
 // Global Variables
 //-----------------------------------------------------------------------------
 
-volatile U32 a_rise;
 volatile U32 a_fall;
 volatile U32 b_rise;
 volatile U32 b_fall;
 volatile U32 timer;
 volatile U32 period;
+
+
+volatile U32 a_duty;
+volatile U32 b_duty;
+volatile U32 a_b_phase;
+volatile U32 freq;
 
 volatile U32 convert;
 volatile U8 state;
@@ -47,23 +63,21 @@ volatile U8 state;
 void main (void){    
    //--------------------------------------------------------------------------
    // Variables
-   //--------------------------------------------------------------------------
-   U32 convert; 
+   //-------------------------------------------------------------------------- 
    U32 i;
    U8 send;
+   U8 cmd;
    //--------------------------------------------------------------------------
    // Initialise
    //--------------------------------------------------------------------------
-   
+  
+   cmd    = ' ';  // Init command
    LED0   = 0;
    LED1   = 0;
-   timer  = 0;
-   period = 1000;
-   a_rise = 500;
-   a_fall = 999;
-   b_rise = 500;
-   b_fall = 999;
+   timer  = 0; 
    state  = 0;
+   freq   = FREQ_MIN; 
+   
    //--------------------------------------------------------------------------
    // Setup micro
    //--------------------------------------------------------------------------
@@ -110,55 +124,63 @@ void main (void){
    // Main loop
    //--------------------------------------------------------------------------
    while(1){     
+      
       if(SCON0_RI){
-         switch(uartRx()){
-            case 'q': if(period != 0xFFFFFFFF){
-                        period += PSTEP;
-                      }
-                      break;
-            case 'a': if(period != 0){
-                        period -= PSTEP;
-                      }
-                      break;
-            case 'w': if(a_rise != 0xFFFFFFFF){
-                        a_rise += PSTEP;
-                      }
-                      break;
-            case 's': if(a_rise != 0){
-                        a_rise -= PSTEP;
-                      }
-                      break;
-            case 'e': if(a_fall != 0xFFFFFFFF){
-                        a_fall += PSTEP;
-                      }
-                      break;
-            case 'd': if(a_fall != 0){
-                        a_fall -= PSTEP;
-                      }
-                      break;
-            case 'r': if(b_rise != 0xFFFFFFFF){
-                        b_rise += PSTEP;
-                      }
-                      break;
-            case 'f': if(b_rise != 0){
-                        b_rise -= PSTEP;
-                      }
-                      break;
-            case 't': if(a_fall != 0xFFFFFFFF){
-                        b_fall += PSTEP;
-                      }
-                      break;
-            case 'g': if(b_fall != 0){
-                        b_fall -= PSTEP;
-                      }
-                      break;
-         }
-
+         cmd=uartRx();
       }
-      if(a_rise > period) a_rise = period;
-      if(a_fall > period) a_fall = period;
-      if(b_rise > period) b_rise = period;
-      if(b_fall > period) b_fall = period;
+     
+      switch(cmd){
+         case 'q':   freq += FREQ_STEP;  
+                     if(freq > FREQ_MAX)  
+                        freq = FREQ_MAX; 
+                     break;
+         case 'a':   freq -= FREQ_STEP;  
+                     if(freq < FREQ_MIN)  
+                        freq = FREQ_MIN; 
+                     break;
+         case 'w':   a_duty += DUTY_STEP;  
+                     if(a_duty > DUTY_MAX)  
+                        a_duty = DUTY_MAX;
+                     break;
+         case 's':   a_duty -= DUTY_STEP;  
+                     if(a_duty < DUTY_MIN)  
+                        a_duty = DUTY_MIN;
+                     break; 
+         case 'e':   b_duty += DUTY_STEP;  
+                     if(b_duty > DUTY_MAX)  
+                        b_duty = DUTY_MAX;
+                     break;
+         case 'd':   b_duty -= DUTY_STEP;  
+                     if(b_duty < DUTY_MIN)  
+                        b_duty = DUTY_MIN;
+                     break;
+         case 'r':   a_b_phase += PHASE_STEP; 
+                     if(a_b_phase > PHASE_MAX) 
+                        a_b_phase = PHASE_MAX;
+                     break;
+         case 'f':   a_b_phase -= PHASE_STEP; 
+                     if(a_b_phase < PHASE_MIN) 
+                        a_b_phase = PHASE_MIN;
+                     break;
+         case ' ':   freq      = FREQ_MIN;
+                     a_duty    = DUTY_MIN;
+                     b_duty    = DUTY_MIN;
+                     a_b_phase = PHASE_MIN; 
+                     break; 
+      } 
+      
+      // Calculate timer numbers   
+      if(0 != cmd){
+         period = (U32)((float)FREQ_TUNE/(float)freq); 
+         a_fall = (U32)(((float)period*((float)a_duty)/DUTY_MAX));
+         b_rise = (U32)(((float)period*((float)a_b_phase)/PHASE_MAX));
+         b_fall = b_rise + (U32)(((float)period*((float)b_duty)/DUTY_MAX));
+         b_fall %= period;
+      }
+
+      // Need another Rx to update
+      cmd = 0;
+
    }
 }
 
@@ -170,7 +192,9 @@ INTERRUPT (TIMER2_ISR, TIMER2_IRQn){
    // Global time keeper
    timer++; 
    timer %= period; 
-   if(timer == a_rise)  PIS0=1;
+   // Drive outputs
+   //    - A always goes high at time 0
+   if(timer == 0)       PIS0=1;
    if(timer == a_fall)  PIS0=0;
    if(timer == b_rise)  PIS1=1;
    if(timer == b_fall)  PIS1=0; 
@@ -180,89 +204,93 @@ INTERRUPT (TIMER2_ISR, TIMER2_IRQn){
 INTERRUPT (TIMER3_ISR, TIMER3_IRQn){           
    U8 send; 
    switch(state){
-      case 0:  uartTx('p');                  break;
-      case 1:  uartTx('e');                  break;
-      case 2:  uartTx('r');                  break;
-      case 3:  uartTx('i');                  break;
-      case 4:  uartTx('o');                  break;
-      case 5:  uartTx('d');                  break; 
-      case 6:  convert=period;
+      case 0:  uartTx('\n');                 break;
+      case 1:  uartTx('\r');                 break; 
+      case 2:  uartTx('F');                  break;
+      case 3:  uartTx('r');                  break;
+      case 4:  uartTx('e');                  break;
+      case 5:  uartTx('q');                  break;
+      case 6:  uartTx('(');                  break;
+      case 7:  uartTx('H');                  break;
+      case 8:  uartTx('z');                  break;
+      case 9:  uartTx(')');                  break;
+      case 10: uartTx('=');                  break; 
+      case 11: convert=freq;
                convert=uartTxPos(convert,6); break;
-      case 7:  convert=uartTxPos(convert,5); break;
-      case 8:  convert=uartTxPos(convert,4); break;
-      case 9:  convert=uartTxPos(convert,3); break;
-      case 10: convert=uartTxPos(convert,2); break;
-      case 11: convert=uartTxPos(convert,1); break;   
-      case 12: convert=uartTxPos(convert,0); break;
-      case 13: uartTx(',');                 break; 
-      case 14: uartTx(' ');                 break;
-      case 15: uartTx('a');                  break;
-      case 16: uartTx('_');                  break;
-      case 17: uartTx('r');                  break;
-      case 18: uartTx('i');                  break;
-      case 19: uartTx('s');                  break;
-      case 20: uartTx('e');                  break; 
-      case 21: convert=a_rise;
-               convert=uartTxPos(convert,6); break;
-      case 22: convert=uartTxPos(convert,5); break;
-      case 23: convert=uartTxPos(convert,4); break;
-      case 24: convert=uartTxPos(convert,3); break;
-      case 25: convert=uartTxPos(convert,2); break;
-      case 26: convert=uartTxPos(convert,1); break;   
-      case 27: convert=uartTxPos(convert,0); break;
-      case 28: uartTx(',');                 break; 
-      case 29: uartTx(' ');                 break;
-      case 30: uartTx('a');                  break;
-      case 31: uartTx('_');                  break;
-      case 32: uartTx('f');                  break;
-      case 33: uartTx('a');                  break;
-      case 34: uartTx('l');                  break;
-      case 35: uartTx('l');                  break; 
-      case 36: convert=a_fall;
-               convert=uartTxPos(convert,6); break;
-      case 37: convert=uartTxPos(convert,5); break;
-      case 38: convert=uartTxPos(convert,4); break;
-      case 39: convert=uartTxPos(convert,3); break;
-      case 40: convert=uartTxPos(convert,2); break;
-      case 41: convert=uartTxPos(convert,1); break;   
-      case 42: convert=uartTxPos(convert,0); break;
-      case 43: uartTx(',');                 break; 
-      case 44: uartTx(' ');                 break;
-      case 45: uartTx('b');                  break;
-      case 46: uartTx('_');                  break;
-      case 47: uartTx('r');                  break;
-      case 48: uartTx('i');                  break;
-      case 49: uartTx('s');                  break;
-      case 50: uartTx('e');                  break; 
-      case 51: convert=b_rise;
-               convert=uartTxPos(convert,6); break;
-      case 52: convert=uartTxPos(convert,5); break;
-      case 53: convert=uartTxPos(convert,4); break;
-      case 54: convert=uartTxPos(convert,3); break;
-      case 55: convert=uartTxPos(convert,2); break;
-      case 56: convert=uartTxPos(convert,1); break;   
-      case 57: convert=uartTxPos(convert,0); break;
-      case 58: uartTx(',');                 break; 
-      case 59: uartTx(' ');                 break;
-      case 60: uartTx('b');                  break;
-      case 61: uartTx('_');                  break;
-      case 62: uartTx('f');                  break;
-      case 63: uartTx('a');                  break;
-      case 64: uartTx('l');                  break;
-      case 65: uartTx('l');                  break; 
-      case 66: convert=b_fall;
-               convert=uartTxPos(convert,6); break;
-      case 67: convert=uartTxPos(convert,5); break;
-      case 68: convert=uartTxPos(convert,4); break;
-      case 69: convert=uartTxPos(convert,3); break;
-      case 70: convert=uartTxPos(convert,2); break;
-      case 71: convert=uartTxPos(convert,1); break;   
-      case 72: convert=uartTxPos(convert,0); break; 
-      case 73: uartTx(13);                 break; 
-      case 74: uartTx('\r');                 break;
+      case 12: convert=uartTxPos(convert,5); break;
+      case 13: convert=uartTxPos(convert,4); break;
+      case 14: convert=uartTxPos(convert,3); break;
+      case 15: uartTx('.');                  break;  
+      case 16: convert=uartTxPos(convert,2); break;
+      case 17: convert=uartTxPos(convert,1); break;   
+      case 18: convert=uartTxPos(convert,0); break;
+      case 19: uartTx(',');                  break;
+      case 20: uartTx('A');                  break;
+      case 21: uartTx(' ');                  break;
+      case 22: uartTx('D');                  break;
+      case 23: uartTx('u');                  break;
+      case 24: uartTx('t');                  break;
+      case 25: uartTx('y');                  break;
+      case 26: uartTx('(');                  break; 
+      case 27: uartTx('%');                  break;
+      case 28: uartTx(')');                  break; 
+      case 29: uartTx('=');                  break; 
+      case 30: convert=a_duty;
+               convert=uartTxPos(convert,5); break;
+      case 31: convert=uartTxPos(convert,4); break;
+      case 32: convert=uartTxPos(convert,3); break;
+      case 33: uartTx('.');                  break;
+      case 34: convert=uartTxPos(convert,2); break;
+      case 35: convert=uartTxPos(convert,1); break;
+      case 36: convert=uartTxPos(convert,0); break;    
+      case 37: uartTx(',');                  break;
+      case 38: uartTx('B');                  break;
+      case 39: uartTx(' ');                  break;
+      case 40: uartTx('D');                  break;
+      case 41: uartTx('u');                  break;
+      case 42: uartTx('t');                  break;
+      case 43: uartTx('y');                  break;
+      case 44: uartTx('(');                  break; 
+      case 45: uartTx('%');                  break;
+      case 46: uartTx(')');                  break; 
+      case 47: uartTx('=');                  break; 
+      case 48: convert=b_duty;
+               convert=uartTxPos(convert,5); break;
+      case 49: convert=uartTxPos(convert,4); break;
+      case 50: convert=uartTxPos(convert,3); break;
+      case 51: uartTx('.');                  break;
+      case 52: convert=uartTxPos(convert,2); break;
+      case 53: convert=uartTxPos(convert,1); break;
+      case 54: convert=uartTxPos(convert,0); break;
+      case 55: uartTx(',');                  break;
+      case 56: uartTx('A');                  break;
+      case 57: uartTx('-');                  break;
+      case 58: uartTx('B');                  break;
+      case 59: uartTx(' ');                  break;
+      case 60: uartTx('P');                  break;
+      case 61: uartTx('h');                  break;
+      case 62: uartTx('a');                  break; 
+      case 63: uartTx('s');                  break;
+      case 64: uartTx('e');                  break; 
+      case 65: uartTx('(');                  break; 
+      case 66: uartTx('d');                  break;
+      case 67: uartTx('e');                  break; 
+      case 68: uartTx('g');                  break; 
+      case 69: uartTx(')');                  break;  
+      case 70: uartTx('=');                  break; 
+      case 71: convert=a_b_phase;
+               convert=uartTxPos(convert,5); break;
+      case 72: convert=uartTxPos(convert,4); break;
+      case 73: convert=uartTxPos(convert,3); break;
+      case 74: uartTx('.');                  break;
+      case 75: convert=uartTxPos(convert,2); break;
+      case 76: convert=uartTxPos(convert,1); break;
+      case 77: convert=uartTxPos(convert,0); break;
+      case 78: uartTx('\r');  
+               state = 1;
+               break;
    }
-   state++;
-   state %= 75;
+   state++; 
    TMR3CN &= ~TMR3CN_TF3H__SET;
 }
 
@@ -274,9 +302,19 @@ INTERRUPT (TIMER3_ISR, TIMER3_IRQn){
 U32 uartTxPos(U32 num, U8 pos){
    U32 scale;
    U8 i;
-   scale = 1;
-   for(i=0;i<pos;i++){
-      scale *= 10;
+   switch(pos){
+      case 0:  scale = 1; break;
+      case 1:  scale = 10; break;
+      case 2:  scale = 100; break;
+      case 3:  scale = 1000; break;
+      case 4:  scale = 10000; break;
+      case 5:  scale = 100000; break;
+      case 6:  scale = 1000000; break;
+      case 7:  scale = 10000000; break;
+      case 8:  scale = 100000000; break;
+      case 9:  scale = 1000000000; break;
+      case 10: scale = 10000000000; break;
+      default: scale = 0; break;
    }
    i = num / scale;         
    uartTx('0'+i);    
